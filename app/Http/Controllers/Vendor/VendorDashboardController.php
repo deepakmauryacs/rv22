86a45b1ae97c3d03331370d576edfd733f89851e
deepakmauryacs <62381959+deepakmauryacs@users.noreply.github.com>
2025-09-16 12:41:35 +0530

    Make vendor dashboard products and ads dynamic

--- app/Http/Controllers/Vendor/VendorDashboardController.php
+++ app/Http/Controllers/Vendor/VendorDashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Vendor;
 
 use App\Http\Controllers\Controller;
+use App\Models\Advertisement;
+use App\Models\VendorProduct;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class VendorDashboardController extends Controller
@@ -11,6 +14,32 @@ class VendorDashboardController extends Controller
     {
         // You can pass user data to the view
         $user = Auth::user();
-        return view('vendor.vendor-dashboard', compact('user'));
+        $recentProducts = collect();
+
+        if ($user) {
+            $recentProducts = VendorProduct::with(['product.division', 'product.category'])
+                ->where('vendor_id', $user->id)
+                ->orderByDesc('created_at')
+                ->take(8)
+                ->get();
+        }
+
+        $today = Carbon::today();
+
+        $advertisements = Advertisement::query()
+            ->where('status', Advertisement::STATUS_ACTIVE)
+            ->where('ad_position', 1)
+            ->where(function ($query) use ($today) {
+                $query->whereNull('validity_period_from')
+                    ->orWhereDate('validity_period_from', '<=', $today);
+            })
+            ->where(function ($query) use ($today) {
+                $query->whereNull('validity_period_to')
+                    ->orWhereDate('validity_period_to', '>=', $today);
+            })
+            ->orderByDesc('id')
+            ->get();
+
+        return view('vendor.vendor-dashboard', compact('user', 'recentProducts', 'advertisements'));
     }
 }
